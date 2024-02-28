70be17d81ffd6007e30d30a943b4650cf2508618
yifanplanet <clothoff@gmail.com>
2024-02-28 02:31:02 -0500

    Add icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,19 @@
 import { AppProps } from "next/app";
 import "@/styles/styles.scss";
 import MobileOverlayWrapper from "./components/MobileOverlay";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MobileOverlayWrapper>
-      <Component {...pageProps} />
-    </MobileOverlayWrapper>
+    <>
+      <Head>
+        <title>Discover Lately</title>
+        <link rel="icon" href="assets/favicon.ico" sizes="any" />
+      </Head>
+      <MobileOverlayWrapper>
+        <Component {...pageProps} />
+      </MobileOverlayWrapper>
+    </>
   );
 }
 
